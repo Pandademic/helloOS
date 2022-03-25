ad0802b430122757b8a634c3d2c2eaa799439084
Atha <74566464+Pandademic@users.noreply.github.com>
2022-03-25 16:13:52 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -3,3 +3,8 @@ stdenv.mkDerivation rec {
         name = "helloOS-dev";
         buildInputs = [ pkgs.mold pkgs.nasm pkgs.gcc pkgs.make pkgs.coreutils ];
 }
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = [ pkgs.buildPackages.mold pkgs.buildPackages.gcc pkgs.buildPackages.make pkgs.buildPackages.coreutils ];
+}
