a8fb2da34a86897ff00be7d364c9a6675962638c
Atha <74566464+Pandademic@users.noreply.github.com>
2022-03-25 16:12:03 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation rec {
         name = "helloOS-dev";
-        buildInputs = [ mold nasm gcc make coreutils ];
+        buildInputs = [ pkgs.mold pkgs.nasm pkgs.gcc pkgs.make pkgs.coreutils ];
 }
