ab35a7ab86041e7ac7bfa2a126044dbb4a728d1d
Atha <74566464+Pandademic@users.noreply.github.com>
2022-03-25 16:19:50 -0400

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
-let
-  pkgs = import <nixpkgs> {};
-
-in pkgs.mkShell rec {
-  name = "helloOS-dev";
-  buildInputs = with pkgs; [pkgs.mold pkgs.coreutils pkgs.nasm pkgs.gcc ];
-  shellHook = ''
-    export PS1='\n\[\033[1;34m\][${name}:\w]\$\[\033[0m\] '
-  '';
+with (import <nixpkgs> {});
+mkShell {
+  buildInputs = [
+    ripgrep
+    mold
+    nasm
+    coreutils
+    curl
+    gcc
+  ];
 }
