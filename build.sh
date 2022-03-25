866593f7162031c168eb2be680b05bd05efacaeb
Atha <74566464+Pandademic@users.noreply.github.com>
2022-03-25 17:09:29 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-nasm -f elf32 boot.asm -o boot.o 
-gcc -m32 -c kernel.c -o kernel.o 
+nasm -f elf32 boot.asm -o boot.o
+gcc -m32 -c kernel.c -o kernel.o
 ld -m elf_i386 -T link.ld -o kernel boot.o kernel.o
